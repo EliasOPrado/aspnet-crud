9bf9de6902892da069e49fe14e2c07053aba1564
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-03-08 07:54:09 -0300

    initial commit

---
+++ todolist/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todolist.Infrainstructure;
+
+namespace todolist.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly TodoContext context;
+        public String Index()
+        {
+            return "test";
+        }
+    }
+}
